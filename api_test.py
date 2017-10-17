1d50da19a3510921910e264a910abbe3cc7f8c4a
deusofnull <wilach@protonmail.com>
2017-10-17 12:50:29 -0400

    api test backup

--- api_test.py
+++ api_test.py
@@ -28,8 +28,9 @@ def _req(url, payload, auth, headers, verify=True):
 def request_minion_info(fields_to_get, nodename):
 	payload = {
 		"query": "SELECT %s FROM view_device_v1 WHERE name = '%s' order by device_pk" % (fields_to_get, nodename), 
-		"headers": "yes"
+		"header": "yes"
 	}
+	print 'generated payload: ' + json.dumps(payload, indent=4) 
 	headers={'Accept': 'application/json'} 
 	auth = ('admin', 'adm!nd42')
 	config = get_config('settings.yaml')
@@ -44,11 +45,32 @@ def main():
 	nodename = 'ubuntu.saltmaster5'
 	
 	response = request_minion_info(mapping, nodename) 
-
+	
 	print(response.text)
-	listrows = response.text.split('\n') 
+	listrows = response.text.split('\n')
 
 	print listrows
+
+	fields = listrows[0].split(',')
+	rows = csv.reader(listrows[1:])
+	out = []
+
+	for row in rows:
+		items = zip(fields, row)
+		item = {}
+		for (name, value) in items:
+			item[name] = value.strip()
+		out.append(item)
+	print "output: " + json.dumps(out, indent=4, sort_keys=True) 
+
+	return out 
+	'''
+	print "\n-------------------- "
+	print("Response: "  + str(response.text))
+	print "\n-------------------- "
+	listrows = response.text.split('\n') 
+
+	# print listrows
 
 	fields = listrows[0].split(',') 
 	rows = csv.reader(listrows[1:])
@@ -65,11 +87,15 @@ def main():
 	# print 'service level: ' + json.dumps(out[0]['service_level'], indent=4, sort_keys=True)
 	
 	return out
+	'''
 
+obj = main()
 
-main()
+# --------------------
+url = "https://10.42.2.241/services/data/v1.0/query/"
 
-'''
+print json.dumps(obj, indent=4, sort_keys=True)
+
 payload = {
     'query': "SELECT * FROM view_device_v1 WHERE name = 'ubuntu.saltmaster5' order by device_pk",
     'header': 'yes'
@@ -102,4 +128,3 @@ for row in rows:
 
 print json.dumps(out, indent=4, sort_keys=True)	
 print 'service level: ' + json.dumps(out[0]['service_level'], indent=4, sort_keys=True)	
-'''
